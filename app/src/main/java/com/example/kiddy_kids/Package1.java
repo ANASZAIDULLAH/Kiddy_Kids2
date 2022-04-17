4895d647f512dd31846966c88a71ddca559e8640
ANASZAIDULLAH <bitm-f19-025@superior.edu.pk>
2022-04-17 22:24:39 +0500

    packages

--- app/src/main/java/com/example/kiddy_kids/Package1.java
+++ app/src/main/java/com/example/kiddy_kids/Package1.java
@@ -5,25 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.kiddy_kids.databinding.ActivityMainBinding;
-
 public class Package1 extends AppCompatActivity {
-
-    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_package1);
 
-        Intent intent = this.getIntent();
 
-        if(intent!=null){
-            String name = intent.getStringExtra("name");
-            String description = intent.getStringExtra("description");
-            String price = intent.getStringExtra("price");
-            int imageid = intent.getIntExtra("imageid",R.drawable.packagepic);
-        }
     }
 }
