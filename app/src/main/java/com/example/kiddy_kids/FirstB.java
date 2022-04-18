1a0de788ad98fbe6d0158d5c1457f64668431dd9
ANASZAIDULLAH <bitm-f19-025@superior.edu.pk>
2022-04-19 00:21:49 +0500

    Categorized Clothes,toys and feeding items

--- app/src/main/java/com/example/kiddy_kids/FirstB.java
+++ app/src/main/java/com/example/kiddy_kids/FirstB.java
@@ -1,14 +1,31 @@
 package com.example.kiddy_kids;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class FirstB extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_b);
+
+        tabLayout= findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.veiwpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addfragment(new ToysFragment(),"TOYS");
+        vpAdapter.addfragment(new FeedingFragment(),"FEEDING");
+        vpAdapter.addfragment(new ClothingFregment(),"CLOTHES");
+        viewPager.setAdapter(vpAdapter);
     }
 }
