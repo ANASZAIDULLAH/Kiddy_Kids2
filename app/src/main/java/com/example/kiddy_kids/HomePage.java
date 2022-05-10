454da85466b9a4136188c870e0ad10e816093258
HP <jawad0318>
2022-05-10 09:29:11 +0500

    Exit  (Anas Zaid)

--- app/src/main/java/com/example/kiddy_kids/HomePage.java
+++ app/src/main/java/com/example/kiddy_kids/HomePage.java
@@ -3,17 +3,22 @@ package com.example.kiddy_kids;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class HomePage extends AppCompatActivity {
-    CardView sale,boys,girls,packages,contactus,aboutus;
+    CardView sale,boys,girls,packages,contactus,aboutus, cart, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+
 
         sale = (CardView) findViewById(R.id.sale);
         sale.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +35,25 @@ public class HomePage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        cart = (CardView) findViewById(R.id.cart);
+        cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),CartSummary.class);
+                startActivity(intent);
+            }
+        });
+
+        logout = (CardView) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         sale = (CardView) findViewById(R.id.sale);
         sale.setOnClickListener(new View.OnClickListener() {
@@ -80,5 +104,27 @@ public class HomePage extends AppCompatActivity {
 
 
 
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomePage.this);
+        builder.setMessage("Do you really want to exit?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finishAffinity();
+            }
+        });
+        builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
